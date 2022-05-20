0aec18dd02a7a9d248754dc377d91b8e839e5b88
David Eaves <null@crc1.net>
2022-05-19 22:44:39 -0400

    my env

---
+++ bootstrap_termux.sh
@@ -0,0 +1,75 @@
+#!/bin/env -S bash
+
+## https://wiki.termux.com/wiki/Recover_a_broken_environment
+# rm -rf /data/data/com.termux/files/usr
+
+# Packages to install
+pkgs=(
+    asciidoctor
+    bc binutils bmon build-essential
+    clamav curl
+    dialog dnsutils
+    exiftool expect
+    fdupes ffmpeg file
+    git gnupg golang
+    htop
+    imagemagick
+    jq
+    libmaxminddb-tools libzmq
+    man moreutils
+    ncurses-utils neofetch nmap nodejs
+    openssh ossp-uuid
+    pdfgrep proot-distro pup python
+    rsync rust
+    screen
+    termux-api
+    toilet
+    vim
+    wget
+)
+
+# Install X11 repo
+pkgs+=("x11-repo")
+
+# X11 packages to install
+case "${pkgs[@]}" in *"x11-repo"*) \
+    pkgs_x11=($(echo $(tr ' ' '\n' <<< "dosbox
+        feh
+        geany
+        hexchat
+        keepassxc
+        netsurf
+        obconf openbox
+        polybar
+        tigervnc
+        wireshark-gtk
+        xfce4-terminal" | sort -u)))
+ ;; esac
+
+# Verbose pkg install
+function pkg_install ()
+{
+    printf "Installing [%s]\n" $@ && echo
+    yes | pkg install $@
+}
+
+# Upgrade termux & install packages
+if type "termux-info" >/dev/null 2>&1; then
+
+    # Upgrade all Termux Packages
+    yes | pkg upgrade && {
+        echo
+
+        # Install regular packages.
+        [[ ! -z "${pkgs}" ]] && {
+            pkg_install ${pkgs[@]}
+            echo
+        }
+
+        # Install X11 packages.
+        [[ ! -z "${pkgs_x11}" ]] && {
+            pkg_install ${pkgs_x11[@]}
+            echo
+        }
+    }
+fi
