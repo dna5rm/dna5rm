de028789a48c22025920b48a5f23a8c24860ff6e
David Eaves <null@crc1.net>
2023-04-05 19:29:40 -0400

    update

--- profile.d/alias.sh
+++ profile.d/alias.sh
@@ -5,3 +5,15 @@ alias pico=nano
 alias rot13="tr a-zA-Z n-za-mN-ZA-M"
 alias toilet="toilet --directory ${HOME}/.fonts/figlet"
 alias vim=nvim
+
+function ask_alpaca()
+{
+    if [[ ! -z "${1}" ]] &&[[ "$(type -t alpaca)" == "file" ]] && [[ -e "${HOME}/models/ggml-alpaca-7b-q4.bin" ]]; then
+        alpaca -m "${HOME}/models/ggml-alpaca-7b-q4.bin" --color --temp 0.9 \
+         --prompt "Write a brief response that appropriately completes the request." \
+         --file <(echo "${1}") 2> /dev/null
+    else
+        printf "${FUNCNAME[0]}: Query the Alpaca LLM...\n\n"
+        echo "Example>> ${FUNCNAME[0]} \"What is $(uname -s)?\""
+    fi
+}
