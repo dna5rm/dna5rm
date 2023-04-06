9d6766ff5a61926e8d5e26514afa8ae58323051d
David Eaves <null@crc1.net>
2023-04-06 09:06:20 -0400

    update

--- profile.d/alias.sh
+++ profile.d/alias.sh
@@ -11,7 +11,7 @@ function ask_alpaca()
     if [[ ! -z "${1}" ]] &&[[ "$(type -t alpaca)" == "file" ]] && [[ -e "${HOME}/models/ggml-alpaca-7b-q4.bin" ]]; then
         alpaca -m "${HOME}/models/ggml-alpaca-7b-q4.bin" --color --temp 0.9 \
          --prompt "Write a brief response that appropriately completes the request." \
-         --file <(echo "${1}") 2> /dev/null
+         --file <(echo "${1}") 2>> "${FUNCNAME[0]}.log" | tee -a "${FUNCNAME[0]}.log"
     else
         printf "${FUNCNAME[0]}: Query the Alpaca LLM...\n\n"
         echo "Example>> ${FUNCNAME[0]} \"What is $(uname -s)?\""
