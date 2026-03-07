0f7888db6147587b3a9afaaef35a4f0d1389be0c
dna5rm <null@crc1.net>
2026-03-07 08:16:18 -0500

    2026-03-07 08:16:18

--- env_python.sh
+++ env_python.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+export arch_pkgs="arch_$(uname -o | tr -cd '[a-zA-Z0-9_\-]' | awk '{print tolower($0)}')"
+
+[[ "${arch_pkgs}" == "arch_android" ]] && {
+    export ANDROID_API_LEVEL=$(getprop ro.build.version.sdk)
+    pip install tokenizers --no-binary :all:
+}
+
 pkgs=(
-    ansible ansible-lint ansible-navigator ansible-vault asn1crypto
+    ansible ansible-lint ansible-navigator asn1crypto
     bcrypt
     cdiff cffi chromaterm cryptography
     f5-sdk fqdn
@@ -14,22 +21,16 @@ pkgs=(
     scp sentencepiece six speedtest-cli snmpclitools
     telegram
     uploadserver
-    xmltodict
-    yamllint yfinance youtube-dl yq
+    yamllint yfinance
 )
 
 django_pkgs=(
-    asgiref
     Django django-crispy-forms django-mathfilters
-    gunicorn
-    sqlparse
-    timeago
+    asgiref gunicorn sqlparse timeago
 )
 
 RPi_pkgs=(
-    RPi.GPIO
-    enviroplus
-    smbus
+    RPi.GPIO enviroplus smbus
 )
 
 arch_android=(
@@ -56,14 +57,13 @@ function pkg_install ()
     pip install $@ 2>> "${HOME}/$(basename ${0%.*}).log"
 }
 
-pip install --upgrade pip setuptools && {
+pip install --upgrade ansible-vault pip setuptools xmltodict yq && {
 
     # Python modules - General
     pkg_install ${pkgs[@]}
     #pkg_install ${django_pkgs[@]}
 
     # Python modules - Arch Specific
-    arch_pkgs=arch_$(uname -o | tr -cd '[a-zA-Z0-9_\-]' | awk '{print tolower($0)}')[@]
     [[ ! -z "${!arch_pkgs}" ]] && { echo; pkg_install ${!arch_pkgs}; }
 
     # Python modules - Rpi
