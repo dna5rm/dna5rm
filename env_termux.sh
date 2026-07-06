9d1c0dd008157c80573a8f51ba42b87213dbe8ff
dna5rm <null@crc1.net>
2026-07-05 20:37:49 -0400

    2026-07-05 20:37:49

--- env_termux.sh
+++ env_termux.sh
@@ -1,77 +1,65 @@
 #!/bin/env -S bash
 
-## https://wiki.termux.com/wiki/Recover_a_broken_environment
-# rm -rf /data/data/com.termux/files/usr
+pkgs=( ncurses-utils tur-repo )
+pkgs+=( ca-certificates clang curl ffmpeg git libffi make python3.11 openssh openssl pkg-config ripgrep rust )
 
-# Packages to install
-pkgs=(
-    asciidoctor
-    bat bc binutils bmon build-essential
-    clamav curl
-    dialog dnsutils
-    exiftool expect
-    fdupes ffmpeg file
-    git glow gnupg golang graphviz
-    htop
-    imagemagick ipcalc
-    jq
-    libandroid-spawn libmaxminddb-tools libxml2 libxslt libzmq lsd
-    man moreutils
-    ncurses-utils neofetch neovim nmap nodejs-lts
-    openssh ossp-uuid
-    pandoc pdfgrep proot-distro pup python
-    rsync rust
-    screen steghide
-    termux-api tidy tmux toilet
-    wget whois
-)
+pkgs+=( argon2 asciidoctor )
+pkgs+=( bat bc binutils bmon build-essential )
+pkgs+=( clamav )
+pkgs+=( dialog dnsutils )
+pkgs+=( exiftool expect )
+pkgs+=( fdupes ffmpeg file )
+pkgs+=( glow gnupg golang graphviz )
+pkgs+=( htop )
+pkgs+=( imagemagick ipcalc )
+pkgs+=( jq )
+pkgs+=( libandroid-spawn libmaxminddb-tools libxml2 libxslt libzmq lsd )
+pkgs+=( mandoc moreutils )
+pkgs+=( ncurses-utils neofetch neovim nmap nodejs-lts )
+pkgs+=( ossp-uuid )
+pkgs+=( pandoc pdfgrep proot-distro pup )
+pkgs+=( rsync )
+pkgs+=( screen steghide )
+pkgs+=( termux-api tidy tmux toilet )
+pkgs+=( wget whois )
 
-# Install X11 repo
-pkgs+=("x11-repo")
+# Functions
+. profile.d/Run-Command.sh
 
-# X11 packages to install
-case "${pkgs[@]}" in *"x11-repo"*) \
-    pkgs_x11=($(echo $(tr ' ' '\n' <<< "dosbox
-        feh
-        geany
-        hexchat
-        keepassxc
-        netsurf
-        obconf openbox
-        polybar
-        tigervnc
-        wireshark-gtk
-        xfce4-terminal" | sort -u)))
- ;; esac
-
-# Verbose pkg install
-function pkg_install ()
-{
-    printf "Installing [%s]\n" $@ && echo
-    yes | pkg install $@
+pipe_check() {
+  input=$(cat)
+  [[ -z "$input" ]] && {
+    echo "${MSG:-Done. }"
+  } || {
+    echo "${input}"
+  }
 }
 
-# Upgrade termux & install packages
-if type "termux-info" >/dev/null 2>&1; then
+# Function Validation
+command -v "Run-Command" >/dev/null 2>&1 || {
+    echo "${0} - Run-Command function failed to load..."
+    exit 0
+}
 
-    # Upgrade all Termux Packages
-    yes | pkg upgrade && {
-        echo
+# Configure Termux
+command -v "termux-info" >/dev/null 2>&1 && {
 
-        # Install regular packages.
-        [[ ! -z "${pkgs}" ]] && {
-            pkg_install ${pkgs[@]}
-            echo
-        }
+  # Install Packages
+  [[ ! -z "${pkgs}" ]] && {
+    for pkg in ${pkgs[@]}; do
+      awk '/^APT-Manual-Installed/{print $NF}' <(apt -o Apt::Cmd::Disable-Script-Warning=true show ${pkg}) \
+      | grep ^"yes"$ > /dev/null \
+        && ( true ) \
+        || ( 
+          printf "\n# Install Missing Package [%s]\n" ${pkg}
+          Run-Command "yes | apt install ${pkg}"
+        )
+    done 2> /dev/null | MSG="Termux packages already installed..." pipe_check
+  }
 
-        # Install X11 packages.
-        [[ ! -z "${pkgs_x11}" ]] && {
-            pkg_install ${pkgs_x11[@]}
-            echo
-        }
-    }
-fi
+  # Python3.11 Virtual Env
+  PY_VENV="$(awk '{ gsub(/\./, "_"); print "venv"$NF}' <(python3.11 --version))"
+  [[ ! -d "${HOME}/.local" ]] && Run-Command "mkdir \"${HOME}/.local\""
+  [[ ! -d "${HOME}/.local/${PY_VENV}" ]] && Run-Command "python3.11 -m venv ~/.local/${PY_VENV}"
 
-# Finished, cleaning up.
-yes | apt autoremove
-yes | apt autoclean
+}
